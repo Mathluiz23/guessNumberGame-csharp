7841813ec6ad5e46dda98c81fa45bbbf76eb7f4c
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-11-11 18:29:20 -0300

    :white_check_mark: test: Implementando teste que irá verificar se a função RandomNumber está entre os valores minimo e máximo solicitados

--- src/guessing-number.Test/TestSecondReq.cs
+++ src/guessing-number.Test/TestSecondReq.cs
@@ -10,30 +10,34 @@ namespace guessing_number.Test;
 [Collection("Sequential")]
 public class TestSecondReq
 {
-    [Theory(DisplayName = "Deve escolher randomicamente um número entre -100 e 100!")]
-    [InlineData(-100, 100)]
-    public void TestRandomlyChooseANumber(int MinimumRange, int MaximumRange)
-    {
-        throw new NotImplementedException();
-    }
+  [Theory(DisplayName = "Deve escolher randomicamente um número entre -100 e 100!")]
+  [InlineData(-100, 100)]
+  public void TestRandomlyChooseANumber(int MinimumRange, int MaximumRange)
+  {
+    var guessNumber = new GuessNumber();
 
-    [Theory(DisplayName = "Deve comparar a entrada do usuário em um caso MENOR")]
-    [InlineData(50, 0)]
-    public void TestProgramComparisonValuesLess(int mockValue, int entry)
-    {
-        throw new NotImplementedException();   
-    }
-    [Theory(DisplayName = "Deve comparar a entrada do usuário em um caso MAIOR")]
-    [InlineData(50, 60)]
-    public void TestProgramComparisonValuesBigger(int mockValue, int entry)
-    {
-        throw new NotImplementedException();  
-    }
-    
-    [Theory(DisplayName = "Deve comparar a entrada do usuário em um caso MAIOR")]
-    [InlineData(50, 50)]
-    public void TestProgramComparisonValuesEqual(int mockValue, int entry)
-    {
-        throw new NotImplementedException();
-    }    
+    guessNumber.RandomNumber();
+
+    guessNumber.randomValue.Should().BeGreaterThan(MinimumRange).And.BeLessThan(MaximumRange);
+  }
+
+  [Theory(DisplayName = "Deve comparar a entrada do usuário em um caso MENOR")]
+  [InlineData(50, 0)]
+  public void TestProgramComparisonValuesLess(int mockValue, int entry)
+  {
+    throw new NotImplementedException();
+  }
+  [Theory(DisplayName = "Deve comparar a entrada do usuário em um caso MAIOR")]
+  [InlineData(50, 60)]
+  public void TestProgramComparisonValuesBigger(int mockValue, int entry)
+  {
+    throw new NotImplementedException();
+  }
+
+  [Theory(DisplayName = "Deve comparar a entrada do usuário em um caso MAIOR")]
+  [InlineData(50, 50)]
+  public void TestProgramComparisonValuesEqual(int mockValue, int entry)
+  {
+    throw new NotImplementedException();
+  }
 }
