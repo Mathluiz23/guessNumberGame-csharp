2a98a5f405dbaa5e02588ef7d4fe6cc5c87123e8
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-11-11 17:47:09 -0300

    :white_check_mark: test: Implementando teste da função Greet, que irá verificar a mensagens e o conteúdos das mensagens via console

--- src/guessing-number.Test/TestFirstReq.cs
+++ src/guessing-number.Test/TestFirstReq.cs
@@ -9,31 +9,42 @@ namespace guessing_number.Test;
 [Collection("Sequential")]
 public class TestFirstReq
 {
-    [Theory(DisplayName = "Deve exibir mensagem inicial!")]
-    [InlineData(new object[] {new string[]{"---Bem-vindo ao ...---", "Para começar..."}})]
-    public void TestPrintInitialMessage(string[] expected)
+  [Theory(DisplayName = "Deve exibir mensagem inicial!")]
+  [InlineData(new object[] { new string[] { "---Bem-vindo ao ...---", "Para começar..." } })]
+  public void TestPrintInitialMessage(string[] expected)
+  {
+    using (var stringWriter = new StringWriter())
     {
-        throw new NotImplementedException();                                                   
-    }
+      Console.SetOut(stringWriter);
+      var guessNumber = new GuessNumber();
+      guessNumber.Greet();
 
-    [Theory(DisplayName = "Deve receber a entrada do usuário e converter para int")]
-    [InlineData("0", 0)]
-    public void TestReceiveUserInputAndConvert(string entry, int expected)
-    {        
-        throw new NotImplementedException();
-    }
+      var response = stringWriter.ToString().Trim().Split("\n");
 
-    [Theory(DisplayName = "Deve receber a entrada do usuário e garantir que é um numérico")]
-    [InlineData(new object[] {new string[]{"10,", "10"}, 10})]
-    public void TestReceiveUserInputAndVerifyType(string[] entrys, int expected)
-    {
-        throw new NotImplementedException();  
-    }
+      response[0].Should().Be(expected[0]);
+      response[1].Should().Be(expected[1]);
 
-    [Theory(DisplayName = "Deve receber a entrada do usuário e garantir que está entre -100 e 100!")]
-    [InlineData(new object[] {new string[]{"1000", "10"}, 10})]
-    public void TestReceiveUserInputAndVerifyRange(string[] entrys, int expected)
-    {
-        throw new NotImplementedException();
-    }    
+    }
+  }
+
+  [Theory(DisplayName = "Deve receber a entrada do usuário e converter para int")]
+  [InlineData("0", 0)]
+  public void TestReceiveUserInputAndConvert(string entry, int expected)
+  {
+    throw new NotImplementedException();
+  }
+
+  [Theory(DisplayName = "Deve receber a entrada do usuário e garantir que é um numérico")]
+  [InlineData(new object[] { new string[] { "10,", "10" }, 10 })]
+  public void TestReceiveUserInputAndVerifyType(string[] entrys, int expected)
+  {
+    throw new NotImplementedException();
+  }
+
+  [Theory(DisplayName = "Deve receber a entrada do usuário e garantir que está entre -100 e 100!")]
+  [InlineData(new object[] { new string[] { "1000", "10" }, 10 })]
+  public void TestReceiveUserInputAndVerifyRange(string[] entrys, int expected)
+  {
+    throw new NotImplementedException();
+  }
 }
