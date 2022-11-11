78b3518bef5564c289de5f51f54c473b257c0e89
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-11-11 17:45:31 -0300

    :sparkles: feat: Implementando a função Greet, que irá escrever 2 frases no console

--- src/guessing-number/GuessingGame.cs
+++ src/guessing-number/GuessingGame.cs
@@ -3,37 +3,38 @@ using System;
 namespace guessing_number;
 
 public class GuessNumber
-{    
-    private IRandomGenerator random;    
-    public GuessNumber() : this(new DefaultRandom()){}
-    public GuessNumber(IRandomGenerator obj)
-    {
-        this.random = obj;
-    }
+{
+  private IRandomGenerator random;
+  public GuessNumber() : this(new DefaultRandom()) { }
+  public GuessNumber(IRandomGenerator obj)
+  {
+    this.random = obj;
+  }
 
-    //user variables
-    public string? response;
-    public int userValue;
-    public int randomValue;
+  //user variables
+  public string? response;
+  public int userValue;
+  public int randomValue;
 
 
-    public void Greet()
-    {
-        throw new NotImplementedException();
-    }
+  public void Greet()
+  {
+    Console.WriteLine("---Bem-vindo ao Guessing Game---");
+    Console.WriteLine("Para começar, tente adivinhar o número que eu pensei, entre -100 e 100!");
+  }
 
-    public void ChooseNumber()
-    {
-        throw new NotImplementedException();     
-    }
-    
-    public void RandomNumber()
-    {
-        throw new NotImplementedException();
-    }
-    
-    public void AnalyzePlay()
-    {
-        throw new NotImplementedException();
-    }
+  public void ChooseNumber()
+  {
+    throw new NotImplementedException();
+  }
+
+  public void RandomNumber()
+  {
+    throw new NotImplementedException();
+  }
+
+  public void AnalyzePlay()
+  {
+    throw new NotImplementedException();
+  }
 }
