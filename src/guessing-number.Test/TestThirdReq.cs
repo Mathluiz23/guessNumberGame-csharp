935e3cdb9e0b05a3cf3db29782e7b04d953c01e0
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-11-12 16:22:58 -0300

    :white_check_mark: test: Implementando teste TestFullFlow, onde irá validar toda a execuçaõ do programa e verificar o fluxo de mensagem fazendo a comparação do número informado pelo usuário com o número gerado aleatóriamente

--- src/guessing-number.Test/TestThirdReq.cs
+++ src/guessing-number.Test/TestThirdReq.cs
@@ -10,10 +10,33 @@ namespace guessing_number.Test;
 [Collection("Sequential")]
 public class TestThirdReq
 {
-    [Theory(DisplayName = "Deve receber a executar o fluxo completo do programa")]
-    [InlineData(new object[] {new string[]{"10"}, 10})]
-    public void TestFullFlow(string[] entrys, int mockValue)
+  [Theory(DisplayName = "Deve receber a executar o fluxo completo do programa")]
+  [InlineData(new object[] { new string[] { "10" }, 10 })]
+  public void TestFullFlow(string[] entrys, int mockValue)
+  {
+    if (Convert.ToInt32(entrys[entrys.Length - 1]) != mockValue) throw new System.OutOfMemoryException();
+
+    using (var stringWriter = new StringWriter())
     {
-        throw new NotImplementedException();
+      using (var stringReader = new StringReader(String.Join("\n", entrys)))
+      {
+        Console.SetIn(stringReader);
+        Console.SetOut(stringWriter);
+
+        var guessNumber = new GuessNumber();
+        guessNumber.Greet();
+        guessNumber.ChooseNumber();
+        guessNumber.randomValue = mockValue;
+        guessNumber.AnalyzePlay();
+
+
+        var response = stringWriter.ToString().Trim().Split("\n");
+
+        if (guessNumber.randomValue < guessNumber.userValue) response[response.Length - 1].Should().Be("Tente um número MENOR");
+        else if (guessNumber.randomValue > guessNumber.userValue) response[response.Length - 1].Should().Be("Tente um número MAIOR");
+        else response[response.Length - 1].Should().Be("ACERTOU!");
+
+      }
     }
+  }
 }
